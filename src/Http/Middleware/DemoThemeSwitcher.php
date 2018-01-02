fa54b5b4a63cbad0c31a2b053c301139c734bebc
Tony Lea <tnylea@gmail.com>
2018-01-01 21:13:10 -0800

    Adding latest content

--- src/Http/Middleware/DemoThemeSwitcher.php
+++ src/Http/Middleware/DemoThemeSwitcher.php
@@ -3,6 +3,7 @@
 namespace DevDojo\App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Schema;
 
 class DemoThemeSwitcher
 {
@@ -15,11 +16,13 @@ class DemoThemeSwitcher
      */
     public function handle($request, Closure $next)
     {
-        if(setting('site.demo_mode', 0)){
-            if(isset($request->theme) && $request->is('/')){
-                $theme = \VoyagerThemes\Models\Theme::where('folder', '=', $request->theme)->first();
-                if(isset($theme->id)){
-                    return redirect('/')->withCookie('voyager_theme', $request->theme);
+        if(Schema::hasTable('settings')){
+            if(setting('site.demo_mode', 0)){
+                if(isset($request->theme) && $request->is('/')){
+                    $theme = \VoyagerThemes\Models\Theme::where('folder', '=', $request->theme)->first();
+                    if(isset($theme->id)){
+                        return redirect('/')->withCookie('voyager_theme', $request->theme);
+                    }
                 }
             }
         }
--- src/Http/Middleware/DynamicConfigs.php
+++ src/Http/Middleware/DynamicConfigs.php
@@ -3,6 +3,7 @@
 namespace DevDojo\App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Schema;
 
 class DynamicConfigs
 {
@@ -15,31 +16,33 @@ class DynamicConfigs
      */
     public function handle($request, Closure $next)
     {
-        $config_map = array(
-            'site.title' => 'app.name',
-            'site.url' => 'app.url',
+        if(Schema::hasTable('settings')){
+            $config_map = array(
+                'site.title' => 'app.name',
+                'site.url' => 'app.url',
 
-            'mail.driver' => 'mail.driver',
-            'mail.host' => 'mail.host',
-            'mail.port' => 'mail.port',
-            'mail.username' => 'mail.username',
-            'mail.password' => 'mail.password',
-            'mail.encryption' => 'mail.encryption',
-            'mail.mailgun_domain' => 'services.mailgun.domain',
-            'mail.mailgun_secret' => 'services.mailgun.secret'
-        );
+                'mail.driver' => 'mail.driver',
+                'mail.host' => 'mail.host',
+                'mail.port' => 'mail.port',
+                'mail.username' => 'mail.username',
+                'mail.password' => 'mail.password',
+                'mail.encryption' => 'mail.encryption',
+                'mail.mailgun_domain' => 'services.mailgun.domain',
+                'mail.mailgun_secret' => 'services.mailgun.secret'
+            );
 
-        foreach($config_map as $key => $config){
-            if(setting($key)){
-                config([$config => setting($key)]);
+            foreach($config_map as $key => $config){
+                if(setting($key)){
+                    config([$config => setting($key)]);
+                }
             }
-        }
 
-        // specific for Debug setting
-        if(setting('site.debug')){
-            config(['app.debug' => true]);
-        } else {
-            config(['app.debug' => false]);
+            // specific for Debug setting
+            if(setting('site.debug')){
+                config(['app.debug' => true]);
+            } else {
+                config(['app.debug' => false]);
+            }
         }
         
         return $next($request);
